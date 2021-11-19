f3a85bba81aae4d7ee75a8dfa6d751e75e445c57
f0lson <forrestrolson@gmail.com>
2021-11-18 19:03:41 -0600

    Successful test - repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
-
+// create function, param: string {string}, num {positive integer}
+const repeatString = function(string, num) {
+    // test if num > 0
+    if (num < 0) {
+        //  if < 0, then return 'ERROR'
+        return 'ERROR';
+    } else {
+        // init totalString {empty string}
+        let totalString = '';
+        // construct string
+        //  - for loop, i < num
+        for (let i = 0; i < num; i++) {
+            totalString += string;
+        }
+        // return totalString
+        return totalString;
+    }
 };
 
 // Do not edit below this line
