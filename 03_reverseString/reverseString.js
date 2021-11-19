60ffb73fb58584b18f8cf361e191cde1df34c456
f0lson <forrestrolson@gmail.com>
2021-11-18 19:29:17 -0600

    Successful test - reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
+// function @param: str {string}
 const reverseString = function(str) {
-
+    // init var stringReverse {string}
+    let stringReverse = '';
+    // reverse loop through str
+    // - find length, subtract 1 (index starts at 0)
+    // - start with last letter
+    // - decrement until i >= 0
+    for (let i = str.length - 1; i >= 0; i--) {
+        // build string, store in stringReverse
+        stringReverse += str[i];
+    }
+    // return stringReverse
+    return stringReverse;
 };
 
 // Do not edit below this line
